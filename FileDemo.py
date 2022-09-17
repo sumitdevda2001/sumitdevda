fa7b1e57fdf4831ea5aee4b4e554731f3dc5de6b
sumitdevda2001 <sumitdevda2001@gmail.com>
2022-09-17 15:19:02 +0530

    Fourth Commit

--- FileDemo.py
+++ FileDemo.py
@@ -1,2 +1,6 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+file=open("tops1.txt","w+")
+file.write("This is w+ operation using python.")
+print("File Current Position : ",file.tell())
+file.seek(10)
+print(file.read())
+file.close()
---
+++ OddEven.py
@@ -0,0 +1,37 @@
+import random
+
+data=open("data.txt","w")
+for i in range(10):
+    num=random.randint(1,100)
+    data.write(str(num)+",")
+data.close()
+
+data=open("data.txt","r")
+even=open("even.txt","w")
+odd=open("odd.txt","w")
+
+l=data.read().split(",")[:-1]
+for i in l:
+    if int(i)%2==0:
+        even.write(i+",")
+    else:
+        odd.write(i+",")
+        
+data.close()
+even.close()
+odd.close()
+
+print("Data File Content")
+data=open("data.txt","r")
+print(data.read())
+data.close()
+
+print("Even File Content")
+even=open("even.txt","r")
+print(even.read())
+even.close()
+
+print("Odd File Content")
+odd=open("odd.txt","r")
+print(odd.read())
+odd.close()
